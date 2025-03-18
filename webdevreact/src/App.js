477c1f2acfa7140f5cee95fad6004ce1cf474b88
mimiyu5 <santillanloriemae@gmail.com>
2025-03-18 13:37:47 +0800

    act6

---
+++ webdevreact/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ {
+    return (
+      <div >
+        <h1>Hello, My Name is Lorie mae Santillan </h1>
+        <h1>Section: A</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
